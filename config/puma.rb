86af413e1516b85490f31611bd0cabffd7ec9f06
Alexander Zubkov <info@zubkov.info>
2024-01-20 19:47:22 +0300

    Dockerize

--- config/puma.rb
+++ config/puma.rb
@@ -18,12 +18,17 @@ if ENV["RAILS_ENV"] == "production"
   workers worker_count if worker_count > 1
 end
 
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+if Rails.env.development?
+  bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
+else
+  port ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
