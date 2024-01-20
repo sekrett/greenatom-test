b63330d77c93c64cef140dd8c108a8e21541fd72
Alexander Zubkov <info@zubkov.info>
2024-01-20 20:06:21 +0300

    Add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    if bearer_token != ENV['API_BEARER_TOKEN']
+      render json: { error: 'Invalid token' }, status: :unauthorized
+    end
+  end
+
+  def bearer_token
+    request.headers['Authorization']&.split('Bearer ')&.last
+  end
 end
